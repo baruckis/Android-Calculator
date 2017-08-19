80882ee5d88066c2759527362dc1741c2323ff89
Andrius Baruckis <andrius.baruckis@gmail.com>
2017-08-19 13:35:07 +0300

    Initial commit

---
+++ app/src/main/java/com/baruckis/calculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.baruckis.calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
